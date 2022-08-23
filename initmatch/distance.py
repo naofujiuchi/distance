4461f1a7de02bee1aa15164359100c07b5da0d32
naofujiuchi <naofujiuchi@gmail.com>
2022-08-23 19:22:26 +0900

    default: missing value is nan

--- initmatch/distance.py
+++ initmatch/distance.py
@@ -7,20 +7,20 @@ from sklearn.metrics import DistanceMetric
 
 class Initmatch():
 
-    def __init__(self, X, Y, penalty = 0, isnan = False):
+    def __init__(self, X, Y, penalty = 0):
         """
         Parameters
         ----------
         penalty: float
             The magnitude of penalty for "element lack".
             There is penalty, which magnitude is determined by the parameter "penalty", for "element lack".
-            If there is a numeric data for an element of one data and there is no numeric data (None) for an element of the other data, then the case is called "element lack".
+            If there is a numeric data for an element of one data and there is no numeric data (nan) for an element of the other data, then the case is called "element lack".
         """
         self.X = copy.deepcopy(X) # 1D array of test data
         self.Y = copy.deepcopy(Y) # 2D array of template data
-        if isnan == True: # Replace np.nan with None
-            self.X = [None if x != x else x for x in self.X]
-            self.Y = [[None if x != x else x for x in row] for row in self.Y]
+        # if isnan == False: # Replace np.nan with None
+        #     self.X = [None if x != x else x for x in self.X]
+        #     self.Y = [[None if x != x else x for x in row] for row in self.Y]
         self.nrow = len(self.Y) # Number of template data
         self.X = [self.X for i in range(self.nrow)] # Making 2D array of test data by replicating the given 1D array test data
         statusX = [[item != None for item in row] for row in self.X]
@@ -31,13 +31,13 @@ class Initmatch():
         statusXY2 = [[item == 2 for item in row] for row in statusXY]
         statusXY12 = [[item in [1,2] for item in row] for row in statusXY]
 
-        # Keeping the values where the both elements are avilable. The other values are replaced with 0.
-        self.X = [[999999 if x == None else x for x in row] for row in self.X]
-        self.X = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.X, statusXY2)]
-        # self.X = np.prod(np.array(self.X, statusXY2), axis = 0)
-        self.Y = [[999999 if x == None else x for x in row] for row in self.Y]
-        self.Y = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.Y, statusXY2)]
-        # self.Y = np.prod(np.array(self.Y, statusXY2), axis = 0)
+        # # Keeping the values where the both elements are avilable. The other values are replaced with 0.
+        # self.X = [[999999 if x == None else x for x in row] for row in self.X]
+        # self.X = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.X, statusXY2)]
+        # # self.X = np.prod(np.array(self.X, statusXY2), axis = 0)
+        # self.Y = [[999999 if x == None else x for x in row] for row in self.Y]
+        # self.Y = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.Y, statusXY2)]
+        # # self.Y = np.prod(np.array(self.Y, statusXY2), axis = 0)
 
         # Calculating penalty
         statusXY1sum = [np.sum(x) for x in statusXY1]
@@ -45,11 +45,12 @@ class Initmatch():
         self.penalty = [x * penalty for x in statusXY1sum] # Value of penalty for each combination of X and Y
 
         # Calculating variance of each axis of template data (Y)
-        self.YforV = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.Y, statusXY12)]
-        # self.YforV = np.prod(np.array(self.Y, statusXY12), axis = 0)
-        npYforV = np.array(self.YforV)
-        maskedYforV = np.ma.masked_where(npYforV == 0, npYforV)
-        self.V = np.var(maskedYforV, axis=0)
+        # self.YforV = [[np.prod(x) for x in zip(*rows)] for rows in zip(self.Y, statusXY12)]
+        # # self.YforV = np.prod(np.array(self.Y, statusXY12), axis = 0)
+        # npYforV = np.array(self.YforV)
+        # maskedYforV = np.ma.masked_where(npYforV == 0, npYforV)
+        # self.V = np.var(maskedYforV, axis=0)
+        self.V = [np.ma.masked_invalid(col).var() for col in zip(*self.Y)]
 
     def distance(self):
         # dist = DistanceMetric.get_metric('seuclidean', V = self.V)
